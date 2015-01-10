9ad8319b6cf8abaa7840e95a83e2f3de3e6ee41f
Michael Borko <michael@borko.at>
2015-01-10 12:06:53 +0100

    added TM4C..

---
+++ build/master_slave_loopback.d
@@ -0,0 +1,19 @@
+build/master_slave_loopback.o: src/master_slave_loopback.c \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h \
+ /home/schueler/opt/tivaware/inc/hw_i2c.h \
+ /home/schueler/opt/tivaware/inc/hw_memmap.h \
+ /home/schueler/opt/tivaware/inc/hw_types.h \
+ /home/schueler/opt/tivaware/driverlib/gpio.h \
+ /home/schueler/opt/tivaware/driverlib/i2c.h \
+ /home/schueler/opt/tivaware/driverlib/pin_map.h \
+ /home/schueler/opt/tivaware/driverlib/sysctl.h \
+ /home/schueler/opt/tivaware/driverlib/uart.h \
+ /home/schueler/opt/tivaware/utils/uartstdio.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdarg.h
---
+++ build/uartstdio.d
@@ -0,0 +1,21 @@
+build/uartstdio.o: /home/schueler/opt/tivaware/utils/uartstdio.c \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h \
+ /home/schueler/opt/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdarg.h \
+ /home/schueler/opt/tivaware/inc/hw_ints.h \
+ /home/schueler/opt/tivaware/inc/hw_memmap.h \
+ /home/schueler/opt/tivaware/inc/hw_types.h \
+ /home/schueler/opt/tivaware/inc/hw_uart.h \
+ /home/schueler/opt/tivaware/driverlib/debug.h \
+ /home/schueler/opt/tivaware/driverlib/interrupt.h \
+ /home/schueler/opt/tivaware/driverlib/rom.h \
+ /home/schueler/opt/tivaware/driverlib/rom_map.h \
+ /home/schueler/opt/tivaware/driverlib/sysctl.h \
+ /home/schueler/opt/tivaware/driverlib/uart.h \
+ /home/schueler/opt/tivaware/utils/uartstdio.h
